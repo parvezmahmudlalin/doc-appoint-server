e54e92393a07a2927f53edbd5f7d0fd1bac9a5a5
Parvez Mahmud Lalin <parvezmahmudlalin@gmail.com>
2026-06-17 23:48:08 +0600

    environment setup

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+
 const app = express();
 
+
 const PORT = 5000;
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server running fine");
